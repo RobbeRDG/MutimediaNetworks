b8693b8a6d514715009ac367563ae5013676aa98
RobbeRDG <robbedegroeve@gmail.com>
2022-05-05 00:41:31 +0200

    Updates

--- main.py
+++ main.py
@@ -69,6 +69,15 @@ def add_padding_to_string(message, n):
     output = ""
     return output.join(message_char_array)
 
+def count_different_symbols(list1, list2):
+    diff_count = 0
+    for i in range(0,len(list1)):
+        if list1[i] != list2[i]:
+            diff_count+=1
+
+    return diff_count
+
+
 def divide_chunks_and_remove_parity(message, n, k):
     # Divide the message in chuncks of length n
     message = divide_chunks(message, n)
@@ -84,7 +93,7 @@ def divide_chunks_and_remove_parity(message, n, k):
 
 # ========================= SOURCE =========================
 # Select an image
-IMG_NAME = 'image.jpg'
+IMG_NAME = 'logo.jpg'
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
 IMG_PATH = os.path.join(dir_path, IMG_NAME)  # use absolute path
@@ -96,6 +105,10 @@ print(img)
 # img.show()
 # uncomment if you want to show the histogram of the colors
 # img.show_color_hist()
+
+# Count the amount of bits used for the image
+uncompressed_image_bit = util.uint8_to_bit(img.get_pixel_seq().copy())
+print("Uncompressed bits needed: {}".format(len(uncompressed_image_bit)))
 
 # ================================================================
 
@@ -115,20 +128,23 @@ pixel_freq = collections.Counter(pixel_sequence).items()
 
 # Construct the huffman tree
 huffman_tree = huffman.Tree(pixel_freq)
-print("Huffman tree generation: {}".format(t.toc()))
+print("Huffman tree generation: {}".format(t.toc_str()))
 
 t.tic()
 # TODO print-out the codebook and validate the codebook (include your findings in the report)
 # Get the encode huffman message using the generated tree
-encoded_message_huffman = huffman.encode(huffman_tree.codebook, pixel_sequence)
+encoded_message_huffman_bit = huffman.encode(huffman_tree.codebook, pixel_sequence)
 
-print("Huffman enc: {}".format(t.toc()))
+print("Huffman enc: {}".format(t.toc_str()))
+
+# Count the amount of bits used for the image
+print("Huffman bits needed: {}".format(len(encoded_message_huffman_bit)))
 
 # Add padding to message string
-encoded_message_huffman = add_padding_to_string(encoded_message_huffman,8)
+encoded_message_huffman_bit = add_padding_to_string(encoded_message_huffman_bit, 8)
 
 # Convert the huffman message bitstring into uint8 array
-uint8_stream_huffman = util.bit_to_uint8(encoded_message_huffman)
+uint8_stream_huffman = util.bit_to_uint8(encoded_message_huffman_bit)
 
 # ======================= SOURCE ENCODING ========================
 # ====================== Lempel-Ziv-Welch ========================
@@ -136,10 +152,13 @@ input_lzw = img.get_pixel_seq().copy()
 
 t.tic()
 encoded_message_lzw, dictonary = lzw.encode(input_lzw)
-print("LZW enc: {}".format(t.toc()))
+print("LZW enc: {}".format(t.toc_str()))
 
 # Convert encoded message integer list into a bitstring
 encoded_message_lzw_bit = util.uint32_to_bit(np.array(encoded_message_lzw))
+
+# Count the amount of bits used for the image
+print("LZW bits needed: {}".format(len(encoded_message_lzw_bit)))
 
 # Convert the bitstring into a byte array for channel transmission
 uint8_stream_lzw = util.bit_to_uint8(encoded_message_lzw_bit)
@@ -180,7 +199,7 @@ rs_encoded_message_bit = util.uint8_to_bit(rs_encoded_message_uint8)
 
 # ====================== CHANNEL TRANSMISSION ========================
 t.tic()
-received_message = channel(rs_encoded_message_bit, ber=0.2)
+received_message = channel(rs_encoded_message_bit, ber=0.55)
 t.toc_print()
 
 # ====================== CHANNEL DECODING ========================
@@ -195,24 +214,23 @@ t.tic()
 received_message_uint8_blocks = list(divide_chunks(received_message_uint8, n))
 
 # Get the blocks from the original message
-original_message_uint8_blocks = list(divide_chunks(received_message_uint8, n))
+original_message_uint8_blocks = list(divide_chunks(rs_encoded_message_uint8, n))
 
 # RS Decode the blocks
 rs_decoded_message = StringIO()
 for cnt, (block, original_block) in enumerate(zip(received_message_uint8_blocks, original_message_uint8_blocks)):
     try:
         decoded, ecc = coder.decode_fast(block, return_string=True)
-        assert coder.check(decoded + ecc), "Check not correct"
+        assert coder.check_fast(decoded + ecc), "Check not correct"
         rs_decoded_message.write(str(decoded))
     except rs.RSCodecError as error:
-        #diff_symbols = len(block) - (original_block == block).sum()
+        diff_symbols = count_different_symbols(block, original_block)
         print(
-            F"Error occured after {cnt} iterations of {len(received_message_uint8)}")
-        #print(F"{diff_symbols} different symbols in this block")
+            F"Error occured after {cnt} iterations of {len(received_message_uint8_blocks)}, {diff_symbols} different symbols")
 
 t.toc_print()
 
-#Get the int array representation of the received message string
+# Get the int array representation of the received message string
 rs_decoded_message_uint8 = np.array(
     [ord(c) for c in rs_encoded_message.getvalue()], dtype=np.uint8)
 
@@ -230,29 +248,29 @@ print("DECODING COMPLETE")
 # TODO after everything works, try to simulate the communication model as specified in the assignment
 # ======================= SOURCE DECODING ========================
 # ====================== Lempel-Ziv-Welch ========================
-#t.tic()
-
 # Convert received byte array into a bitstring
-#rs_decoded_message_bit = util.uint8_to_bit(uint8_stream_lzw)
+rs_decoded_message_bit = util.uint8_to_bit(uint8_stream_lzw)
 
 # Convert the bitstring into an int array
-#rs_decoded_message_int = util.bit_to_uint32(rs_decoded_message_bit)
+rs_decoded_message_int = util.bit_to_uint32(rs_decoded_message_bit)
 
-# Decode
-#final_message = lzw.decode(rs_decoded_message_int.tolist())
-
-#print("Enc: {0:.4f}".format(t.toc()))
-# ======================= SOURCE DECODING ========================
-# =========================== Huffman ============================
 t.tic()
 
+# Decode
+final_message = lzw.decode(rs_decoded_message_int.tolist())
+
+print("LZW Dec: {}".format(t.toc_str()))
+# ======================= SOURCE DECODING ========================
+# =========================== Huffman ============================
 # Convert uint8 array to bitstring
-rs_decoded_message_bit = util.uint8_to_bit(rs_decoded_message_uint8_without_parity)
+#rs_decoded_message_bit = util.uint8_to_bit(rs_decoded_message_uint8_without_parity)
+
+#t.tic()
 
 # Decode
-final_message = huffman.decode(huffman_tree, rs_decoded_message_bit)
+#final_message = huffman.decode(huffman_tree, rs_decoded_message_bit)
 
-print("Dec: {}".format(t.toc()))
+#print("Huffman Dec: {}".format(t.toc_str()))
 # ========================= SINK =========================
 # Convert the image array back into an rgb array
 pixel_array = build_rgb_image_from_array(np.array(final_message), img.height, img.width)
